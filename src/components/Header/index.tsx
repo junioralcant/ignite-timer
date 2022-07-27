ead3ac6fdad4fbc6c7119a3d02039bf6f60791c8
junioralcant <junioralcant111@gmail.com>
2022-07-27 10:50:30 -0300

    Routes pages - component header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,20 @@
+import { HeaderContainer } from './styles';
+import { Timer, Scroll } from 'phosphor-react';
+import logoIgnite from '../../assets/logo.svg';
+import { NavLink } from 'react-router-dom';
+
 export function Header() {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <HeaderContainer>
+      <img src={logoIgnite} alt="" />
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
   );
 }
