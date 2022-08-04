2fdbb36097728404e4c1ee1a2e80924db77a1ac0
junioralcant <junioralcant111@gmail.com>
2022-08-04 15:34:29 -0300

    Application functionality - Create countdown

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -37,6 +37,7 @@ export function Home() {
   const [activeCycleId, setActiveCycleId] = useState<string | null>(
     null
   );
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
 
   const { register, handleSubmit, watch, reset } =
     useForm<NewCycleFormData>({
@@ -66,7 +67,19 @@ export function Home() {
     (cycle) => cycle.id === activeCycleId
   );
 
-  console.log(activeCycle);
+  const totalSeconds = activeCycle
+    ? activeCycle.minutesAmount * 60
+    : 0;
+
+  const currentSeconds = activeCycle
+    ? totalSeconds - amountSecondsPassed
+    : 0;
+
+  const minutesAmount = Math.floor(currentSeconds / 60);
+  const secondsAmount = currentSeconds % 60;
+
+  const minutes = String(minutesAmount).padStart(2, '0');
+  const seconds = String(secondsAmount).padStart(2, '0');
 
   const task = watch('task');
   const isSubmitDisabled = !task;
@@ -105,11 +118,11 @@ export function Home() {
         </FormContainer>
 
         <CountdownContainer>
-          <span>0</span>
-          <span>0</span>
+          <span>{minutes[0]}</span>
+          <span>{minutes[1]}</span>
           <Separator>:</Separator>
-          <span>0</span>
-          <span>0</span>
+          <span>{seconds[0]}</span>
+          <span>{seconds[1]}</span>
         </CountdownContainer>
 
         <StartCountdownButton
