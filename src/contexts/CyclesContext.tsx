b196cb5db77fe7b293f4b5fe3a50c8cbbcaec075
junioralcant <junioralcant111@gmail.com>
2022-08-11 17:12:12 -0300

    Reducers - Fix history listing

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,6 +1,8 @@
+import { differenceInSeconds } from 'date-fns';
 import {
   createContext,
   ReactNode,
+  useEffect,
   useReducer,
   useState,
 } from 'react';
@@ -38,18 +40,49 @@ export const CyclesContext = createContext({} as CycleContextType);
 export function CyclesContextProvider({
   children,
 }: CycleContextProviderProps) {
-  const [cyclesState, dispatcher] = useReducer(cyclesReducer, {
-    cycles: [],
-    activeCycleId: null,
-  });
+  const [cyclesState, dispatcher] = useReducer(
+    cyclesReducer,
+    {
+      cycles: [],
+      activeCycleId: null,
+    },
+    () => {
+      const storedStateAsJSON = localStorage.getItem(
+        '@ignite-timer:cycles-state-1.0.0'
+      );
 
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
+      if (storedStateAsJSON) {
+        return JSON.parse(storedStateAsJSON);
+      }
+    }
+  );
 
   const { cycles, activeCycleId } = cyclesState;
-
   const activeCycle = cycles.find(
     (cycle) => cycle.id === activeCycleId
   );
+
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(
+    () => {
+      if (activeCycle) {
+        return differenceInSeconds(
+          new Date(),
+          new Date(activeCycle.startDate)
+        );
+      }
+
+      return 0;
+    }
+  );
+
+  useEffect(() => {
+    const stateJSON = JSON.stringify(cyclesState);
+
+    localStorage.setItem(
+      '@ignite-timer:cycles-state-1.0.0',
+      stateJSON
+    );
+  }, [cyclesState]);
 
   function markCurrentCycleAsFinished() {
     dispatcher(markCurrentCycleAsFinishedAction());
